b412186b20ebbf46c82d061fac30b8e8ab583d6c
Sven Lindqvist <sven-lindqvist@hotmail.com>
2019-10-25 16:29:37 +0200

    Fetch different limetypes, updated webcomponent

--- frontend/src/components/lwc-test-webcomponent-hello-world/lwc-test-webcomponent-hello-world.tsx
+++ frontend/src/components/lwc-test-webcomponent-hello-world/lwc-test-webcomponent-hello-world.tsx
@@ -7,7 +7,7 @@ import {
     HttpService,
 } from '@limetech/lime-web-components-interfaces';
 import { Component, Element, h, Prop, State } from '@stencil/core';
-import { ListItem,ListSeparator } from '@limetech/lime-elements';
+import { ListItem, ListSeparator } from '@limetech/lime-elements';
 
 @Component({
     tag: 'lwc-test-webcomponent-hello-world',
@@ -26,65 +26,109 @@ export class HelloWorld implements LimeWebComponent {
     @Element()
     public element: HTMLElement;
 
-
     @State()
     private section = [{
-        title: null,
+        name: null,
         priority: null,
         misc: null,
-        solutionimprovementstatus: null,
-        isOpen:  false,
     }];
 
 
-    @State()
-    private items: Array<ListItem<number> | ListSeparator> = [];
+
+
 
     @State()
-    private outputList: String;
+    private outputLeftList: String;
+    private outputMiddleList: String;
+    private outputRightList: String;
 
     private http: HttpService;
 
 
-    
+
     public componentWillLoad() {
         this.http = this.platform.get(PlatformServiceName.Http);
         console.log("componentWillLoad");
-        this.http.get(`https://localhost/lime/limepkg-sprint/test/?limetype=solutionimprovement`).then(res => {
-            this.section = res.objects.map(el => {
-                return this.section = {...el};
-            });
-            this.createOutPut();
+        this.getDataFromEndPoint("solutionimprovement")
+
+    }
+
+    private getDataFromEndPoint(limeType) {
+        console.log(limeType)
+        this.http.get(`https://localhost/lime/limepkg-sprint/test/?limetype=` + limeType).then(res => {
+            this.updateData(res);
+            /*         this.section = res.objects.map(el => {
+                            return this.section = { ...el };
+                        });
+                        this.createOutPut(); */
+                        console.log(res);
         }
         );
     }
 
-    private createOutPut() {
-        let index = 1;
-        this.section.forEach(element => {
-            this.items.push({
-                text: element.title,
-                secondaryText: "Status: " + element.solutionimprovementstatus,
-                value: index++,
-            },
-            {separator: true})
-            console.log(element);
-        }
-        )
-         this.outputList = <limel-list items={this.items}/>
+    private updateData = (res) => {
+        this.section = res.objects.map(el => {
+            return this.section = { ...el };
+        });
+        this.createOutPut();
     }
 
-    
+    private createOutPut() {
+        let itemsLeftList: Array<ListItem<any> | ListSeparator> = [];
+        let itemsMiddleList: Array<ListItem<any> | ListSeparator> = [];
+        let itemsRightList: Array<ListItem<any> | ListSeparator> = [];
+        let index = 1;
+        this.section.forEach(element => {
+            const item = {
+                text: element.name,
+                secondaryText: "Priority: " + element.priority,
+                value: index++,
+            }
+            if (element.priority == "urgent" || element.priority == "rejection" || element.priority == "agreement") {
+                itemsLeftList.push(
+                    (item as ListItem),
+                    { separator: true })
+            } else if (element.priority == "wish" ||element.priority == "contact" || element.priority == "prospect") {
+                itemsMiddleList.push(
+                    (item as ListItem),
+                    { separator: true })
+            } else if (element.priority == "better" || element.priority == "agreement" || element.priority == "active") {
+                itemsRightList.push(
+                    (item as ListItem),
+                    { separator: true }
+/*                     {text: element.title,
+                    secondaryText: "Priority: " + element.priority,
+                    value: index++,
+                },
+                    { separator: true } */)
+            }
+        }
+        )
+        this.outputLeftList = <limel-list items={itemsLeftList} />
+        this.outputMiddleList = <limel-list items={itemsMiddleList} />
+        this.outputRightList = <limel-list items={itemsRightList} />
+    }
+
+
     public render() {
         console.log("Render()");
-     
-        console.log(this.items);
-
-
         return (
-            <limel-flex-container direction={"vertical"} align={"stretch"} justify={"space-around"}>
-                <limel-button label="click me" onClick={this.render} />
-                {this.outputList}
+            <limel-flex-container direction={"horizontal"} align={"stretch"} justify={"space-between"}>
+                <limel-flex-container direction={"vertical"} align={"stretch"} justify={"start"}>
+                    <limel-button label="Get deals" onClick={() => this.getDataFromEndPoint("deal")}/>
+                    <h3>Priority Urgent</h3>
+                    {this.outputLeftList}
+                </limel-flex-container>
+                <limel-flex-container direction={"vertical"} align={"stretch"} justify={"space-around"}>
+                    <limel-button label="Get solution improvements" onClick={() => this.getDataFromEndPoint("solutionimprovement")}/>
+                    <h3>Priority Wish</h3>
+                    {this.outputMiddleList}
+                </limel-flex-container>
+                <limel-flex-container direction={"vertical"} align={"stretch"} justify={"end"}>
+                    <limel-button label="Get Companies" onClick={() => this.getDataFromEndPoint("company")}/>
+                    <h3>Priority better get started</h3>
+                    {this.outputRightList}
+                </limel-flex-container>
             </limel-flex-container>
         );
     }
